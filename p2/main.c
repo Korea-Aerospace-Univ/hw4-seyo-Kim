9c393ec45fa6745188ff466721651af5a58b2573
2025050036 <seoyoon6389@kau.kr>
2026-04-15 16:37:49 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -4,38 +4,37 @@ int main(void)
 {
     char C;
     
-    int i, N;
+    int N;
     int cE = 0, tE = 0;
     int cN = 0, tN = 0;
     
     scanf("%d", &N);
     
-    for (i = 0; i < N; i++) {
+    for (int i=0; i<N; i++) {
         
         scanf("%c", &C);
         
-        if (C >= 'a' && C <= 'z') {
+        if (C >='a' && C <='z') {
             cE++;
-            cN = 0;
+            cN=0;
             
             if(cE> tE) {
-                tE = cE;
+                tE=cE;
             }
-            
         }
         
-        else if (C >= '0' && C <= '9') {
+        else if (C >='0' && C <='9') {
             cN++;
-            cE = 0;
+            cE=0;
             
             if(cN> tN) {
-                tN = cN;
+                tN=cN;
             }
         }
         
         else {
-            cE = 0;
-            cN = 0;
+            cE=0;
+            cN=0;
         }
     }
     
